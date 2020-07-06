e2599baff2773799cb2a2f68704ffb60c776ed38
Junaid Jawed Khan <junaidkhan.tmk@hotmail.com>
2020-07-05 18:23:43 -0700

    Defined macros for SPI configuration structure fields.

--- drivers/inc/spi_driver.h
+++ drivers/inc/spi_driver.h
@@ -42,6 +42,70 @@ typedef struct{
 }SPI_Handle_t;
 
 /*
+ *     SPI driver specific MACROs  - 
+ *          these macros are only relevant to SPI driver
+ *
+ */
+
+
+/**
+ *  @DeviceMode
+ */
+
+#define SPI_IN_SLAVE_MODE      0      /* By default, SPIx is in Slave Mode */ 
+#define SPI_IN_MASTER_MODE     1
+
+
+/**
+ *  @BusConfig 
+ */
+#define SPI_IN_FULL_DUPLEX_MODE  0   /* By Default, SPI communication type is Full duplex */ 
+#define SPI_IN_HALF_DUPLEX_MODE  1
+#define SPI_IN_SIMPLEX_RX_ONLY   2
+#define SPI_IN_SIMPLEX_TX_ONLY   3
+
+
+/**
+ *  @SclkSpeed -> Serial Clock Speed, controlled by BaudRate bits of the Control Register
+ *  SPI_PERIPHERAL_CLOCK_DIVx, where x = Internal prescaler value
+ */ 
+#define SPI_PERIPHERAL_CLOCK_DIV2   0 /* By default, SPI serial clock is Peripheral clock /2, i.e. SPI internal prescaler minimum value = 2 */ 
+#define SPI_PERIPHERAL_CLOCK_DIV4   1
+#define SPI_PERIPHERAL_CLOCK_DIV8   2
+#define SPI_PERIPHERAL_CLOCK_DIV16  3
+#define SPI_PERIPHERAL_CLOCK_DIV32  4
+#define SPI_PERIPHERAL_CLOCK_DIV64  5
+#define SPI_PERIPHERAL_CLOCK_DIV128 6
+#define SPI_PERIPHERAL_CLOCK_DIV256 7
+
+/**
+ * @DataFrameFormat 
+ */
+
+#define SPI_DATAFRAME_8BITS 0           /* By Default, SPI communication Data Frame is 8-bits. */ 
+#define SPI_DATAFRAME_16BIT 1
+
+/**
+ * @ClockPolarity
+ */
+#define SPI_SCLK_LOW    0               /* By Default, SPI Serial Clock is LOW at idle state. */ 
+#define SPI_SCLK_HIGH   1 
+
+/**
+ * @ClockPhase
+ */
+#define SPI_DATA_SAMPLED_ON_LEADING_EDGE    0               /* By Default, Data Sampled on First edge of SCLK.  */ 
+#define SPI_DATA_SAMPLED_ON_TRAILING_EDGE   1 
+
+
+/**
+ * @SlaveManagementType
+ */
+#define SPI_SOFTWARE_SLAVE_MANAGEMENT_DI    0           /* By Default, Software Slave Management is Disabled, i.e. SlaveManagementType = Hardware Slave Management. */ 
+#define SPI_SOFTWARE_SLAVE_MANAGEMENT_EN    1
+
+
+/*
  * 					APIs supported by this SPI driver.
  *
  */
