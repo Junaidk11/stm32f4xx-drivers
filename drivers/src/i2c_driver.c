ea9525de30c651591b74ffa78f8e0030214065d6
Junaid Jawed Khan <jjkhan@sfu.ca>
2021-04-03 18:07:16 -0700

    Added I2C_DeInit API definition

--- drivers/src/i2c_driver.c
+++ drivers/src/i2c_driver.c
@@ -7,12 +7,13 @@
 
 #include "i2c_driver.h"
 
+
 /*********************************************************************
  * @fn      		  - I2C_ClockControl
  *
  * @brief             - This API function will be used to enable clock access to one of the I2C peripherals.
  *
- * @param[in]         -  a pointer to the base address of the SPI peripheral we want to configure - I2C_RegDef_t -> points to the base-address of the I2Cx (either I2C1, I2C2, or I2C3)
+ * @param[in]         -  a pointer to the base address of the I2C peripheral we want to configure - I2C_RegDef_t -> points to the base-address of the I2Cx (either I2C1, I2C2, or I2C3)
  * @param[in]         -  a variable to indicate if you wish to enable or disable the port. Define a Generic macro in MCU header file, use it - Use MACRO: Enable or Disable.
  * @param[in]         -
  *
@@ -47,3 +48,29 @@ void I2C_ClockControl(I2C_RegDef_t *pI2Cx,uint8_t enable_disable ){
 		}
 }
 
+
+/*********************************************************************
+ * @fn      		  - I2C_DeInit
+ *
+ * @brief             - Resets the I2Cx registers
+ *
+ * @param[in]         - The Base address of your I2C module - I2C_RegDef_t*
+ * @param[in]         -
+ * @param[in]         -
+ *
+ * @return            -  None
+ *
+ * @Note              -  You can use RCC's AHB1 Peripheral Reset Register.
+   		                 This way, you don't have to reset each every register of the respective I2Cx.
+
+ */
+void I2C_DeInit(I2C_RegDef_t *pI2Cx){
+
+    if(pI2Cx == I2C1){
+        I2C1_REG_RESET();
+    }else if(pI2Cx == I2C2){
+        I2C2_REG_RESET();
+    }else if(pI2Cx == I2C3){
+        I2C3_REG_RESET();
+    }
+}
